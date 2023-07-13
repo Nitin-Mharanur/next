8da65467bc3823a2a5f7d72811cd871448c40ad4
Nitin-Mharanur <nitin.mharanur@rasonix.com>
2023-07-13 15:24:44 +0530

    next app created

--- src/app/Product/[id]/page.jsx
+++ src/app/Product/[id]/page.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
-function page({params.id}) {
-  return <div>This is prodct {{id}}</div>;
+import { useRouter } from "next/navigation";
+
+function page({ params }) {
+  const [response, setResponse] = useState([]);
+  const router = useRouter();
+  useEffect(() => {
+    function fetchData(id) {
+      fetch(`http://localhost:8000/api/services/${id}`)
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+    }
+
+    fetchData(params.id);
+  }, []);
+
+  //   useEffect(() => {
+  //     if (!response.title) {
+  //       //   router.push("/Product/go/1222");
+  //       router.replace("/Product/go/111", undefined, { statusCode: 301 });
+  //     }
+  //   }, []);
+
+  return <div>This is prodct {params.id}</div>;
 }
 
 export default page;
